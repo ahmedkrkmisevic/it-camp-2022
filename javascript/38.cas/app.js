a6bf7752cc4758a14b6873756009238d6bb1e5b1
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2022-11-07 20:58:05 +0100

    practice and end of array methods

---
+++ javascript/38.cas/app.js
@@ -0,0 +1,20 @@
+
+// console.log(saberidvabroja(4, 6))
+// function saberidvabroja(br1, br2) {
+//     return br1 + br2;
+// }
+// console.log(saberitribroja(4, 6, 2));
+// const saberitribroja = (br1, br2, br3) => br1 + br2 + br3;
+
+//hoisting kod funkcija vazi ako se radi o funkcijama definisanim preko function 
+// keyword dok kod arrow funkcija ne vazi tj.
+// nije moguce pozvati funkciju pre definisanja iste.
+
+// da li je neka vrednost truthy ili falsy 
+console.log(!!"muhamed")
+//console.log(boolean("muhamed"));alternativa
+
+console.log(!!(5 + "36"))
+
+const niz = [1, 2, 3, 4, 5, 6, 7,]
+
---
+++ javascript/38.cas/domaci.js
@@ -0,0 +1,13 @@
+const firsttask = () => {
+    const voce = prompt("unesite listu sa vocem:")
+    const nizvoca = voce.split(",");
+    if (nizvoca.includes("jagoda")) {
+        return `Jagoda se nalazi na${nizvoca.indexOf("jagoda") + 1} mestu`
+    } else {
+        nizvoca.sort()
+        nizvoca.unshift(10)
+        nizvoca.push(100)
+        return nizvoca;
+    }
+};
+console.log(firsttask());
