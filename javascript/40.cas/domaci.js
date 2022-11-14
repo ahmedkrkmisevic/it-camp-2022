16943ef2eeffebb0ef35e42aeed80e831a55d3b2
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2022-11-14 20:59:40 +0100

    objects - practice

--- javascript/40.cas/domaci.js
+++ javascript/40.cas/domaci.js
@@ -13,3 +13,42 @@ const student = {
     }
 };
 console.log(student.ProsekOcena());
+
+const myCar = {
+    id: 1,
+    marka: "Audi",
+    model: "a4",
+    boja: "Crvena",
+    pogon: "prednji",
+    menjac: "automatski",
+    kontakt: [0622222, 02033322],
+    servis: {
+        datum: "04,maj",
+        km: 23000,
+        serviser: "Pasovic",
+    },
+    udaran: true,
+    trenutnaBrzina: 0,
+    maksimalnaBrzina: 260,
+    // povecanjeBrzine:function (p) {
+    // {
+
+    //         this.trenutnaBrzina+=p;
+    //         if (this.trenutnaBrzina<=260) {
+    //         console.log( `brzina je ${this.trenutnaBrzina}km/h.`)
+    //         }else{
+    //         console.log(`Ne mozete ici ${this.trenutnaBrzina}km/h jer je vasa maximalna brzina 260km/h`)
+    //     }
+    // }
+    smanjenjeBrzine: function (s) {
+        this.trenutnaBrzina -= s;
+        if (this.trenutnaBrzina >= 0) {
+            console.log(`Vasa brzina je ${this.trenutnaBrzina}km/h.`)
+        } else
+            console.log(`Ne moze ici ${this.trenutnaBrzina}km/h jer je vasa minimalna brzina 0km/h`)
+    }
+};
+
+// myCar.povecanjeBrzine(210);
+myCar.smanjenjeBrzine(30);
+// console.log(myCar.trenutnaBrzina);
