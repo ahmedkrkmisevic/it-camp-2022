74259a32dc4a937678e21b57d3437179ae4a898a
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2022-11-27 16:14:11 +0100

    practice

--- javascript/vezbanje teest/app.js
+++ javascript/vezbanje teest/app.js
@@ -31,4 +31,22 @@ console.log(index3);
 //pop ()metoda se koristi za brisanje poslednjeg elementa niza.
 //unshift() metoda dodaje element na prvom mestu u nizu.
 // shift metoda se kosristi za brisanje prvog elemnta niza
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
---
+++ javascript/vezbanje teest/app2.js
@@ -0,0 +1,44 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
+
+
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+
+
+
+function greeting(name) {
+    alert(`Hello, ${name}`);
+}
+
+function processUserInput(callback) {
+    const name = prompt("Please enter your name.");
+    callback(name);
+}
+
+processUserInput(greeting);// expected output: Array ["Jan", "Feb", "March", "April", "May"]
