10a3c867efd513624922b7b0c01a7fc2c9a33292
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2022-12-10 19:11:38 +0100

    47cas

---
+++ javascript/48.cas/app.js
@@ -0,0 +1,50 @@
+// We have an array of objects representing different people in our contacts lists.
+// A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+// The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+// If both are true, then return the "value" of that property.
+// If name does not correspond to any contacts then return the string No such contact.
+// If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    const objectWithName = contacts.find((contact) => contact.firstName === name);
+    if (objectWithName !== undefined) {
+        if (Object.keys(objectWithName).includes(prop)) {
+            return objectWithName[`${prop}`];
+        } else {
+            return `No such property.`;
+        }
+    } else {
+        return `No such contact.`;
+    }
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Imran", "likes"));
+console.log(lookUpProfile("Kristian", "hobbies"));
