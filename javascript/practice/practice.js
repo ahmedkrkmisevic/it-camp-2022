438e2e1df6587066d0fb9336ff12b5d25b6a9bce
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-08 19:27:13 +0100

    dom

--- javascript/practice/practice.js
+++ javascript/practice/practice.js
@@ -9,3 +9,22 @@ console.log(ime2);
 const voce = ["jabuka", "kruska", "jagoda", "mandarina", "breskva"]
 const voce2 = voce.findIndex((element) => element("jagoda"))
 console.log(voce2);
+// The DOM is a W3C (World Wide Web Consortium) standard.
+
+// The DOM defines a standard for accessing documents:
+
+// "The W3C Document Object Model (DOM) is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content, structure, and style of a document."
+
+// The W3C DOM standard is separated into 3 different parts:
+
+// Core DOM - standard model for all document types
+// XML DOM - standard model for XML documents
+// HTML DOM - standard model for HTML documents
+// The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+// The HTML elements as objects
+// The properties of all HTML elements
+// The methods to access all HTML elements
+// The events for all HTML elements
+// In other words: The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+
