870995a70ea5b441a970016263fbeab7bcc6153f
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-17 18:16:53 +0100

    react

--- my/src/App.js
+++ my/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Styleheader from './components/styleheader/styleheader';
+import StyledButton from './components/styleButton';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Styleheader text="header" > </Styleheader>
+        <StyledButton text="button"></StyledButton>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
