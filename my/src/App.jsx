01eecbc271dd2690c5b20c1f0b39e594cdc13590
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-19 18:42:11 +0100

    hooks

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -37,3 +37,19 @@ const App = () => {
   );
 };
 export default App;
+
+import React, { useState } from "react";
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
