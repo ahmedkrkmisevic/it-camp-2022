4cacb4e0ff71644f63445ce988fbfa0b72c654de
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-20 20:18:00 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -3,9 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Styleheader from "./components/styleheader/styleheader";
 import StyleButton from "./components/styleButton/styleButton";
+import StyledInput from "./components/StyledInput/StyledInput";
 const App = () => {
   const [innerText, setInnerText] = useState("");
   const [clicked, setClicked] = useState(false);
+  const [inputValue, setInputValue] = useState();
 
   useEffect(() => {
     clicked ? setInnerText("clicked") : setInnerText("click me");
@@ -14,6 +16,9 @@ const App = () => {
   function buttonClick() {
     setClicked(!clicked);
   }
+  function onChangeFunction(event) {
+    setInputValue(event.target.value);
+  }
 
   return (
     <div className="App">
@@ -21,6 +26,7 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <Styleheader text="header"> </Styleheader>
         <StyleButton innerText={innerText} onClickHandler={buttonClick} />
+        <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
