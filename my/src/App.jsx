6fbfb06720df41cb19f47dfd2466ba43ac16486b
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-21 17:35:49 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,21 +1,58 @@
+// import React, { useState, useEffect } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Styleheader from "./components/styleheader/styleheader";
+// import StyleButton from "./components/styleButton/styleButton";
+// import StyledInput from "./components/StyledInput/StyledInput";
+// import StyledForm from "./components/StyledForm/StyledForm";
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Styleheader text="header"> </Styleheader>
+//         <StyledForm />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// };
+// export default App;
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import Styleheader from "./components/styleheader/styleheader";
-import StyleButton from "./components/styleButton/styleButton";
-import StyledInput from "./components/StyledInput/StyledInput";
-import StyledForm from "./components/StyledForm/StyledForm";
+import StyledButton from "./components/StyledButton/StyledButton";
+import CustomForm from "./components/CustomForm/CustomForm";
 
 const App = () => {
+  const [innerText, setInnerText] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    clicked ? setInnerText("Clicked") : setInnerText("Click me");
+  }, [clicked]);
+
+  function buttonClick() {
+    setClicked(!clicked);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Styleheader text="header"> </Styleheader>
-        <StyledForm />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <StyledButton innerText={innerText} onClickHandler={buttonClick} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,8 +61,8 @@ const App = () => {
         >
           Learn React
         </a>
+        <CustomForm />
       </header>
     </div>
   );
 };
-export default App;
--- my/src/components/StyledForm/StyledForm.jsx
+++ my/src/components/StyledForm/StyledForm.jsx
@@ -1,31 +1,57 @@
-import React from "react";
-import StyleButton from "../styleButton/styleButton";
-import "./StyledForm.css";
+// import React from "react";
+// import StyleButton from "../styleButton/styleButton";
+// import "./StyledForm.css";
+// import StyledInput from "../StyledInput/StyledInput";
+// import { useState, useEffect } from "react";
+
+// const StyledForm = (props) => {
+//   const [innerText, setInnerText] = useState("");
+//   const [clicked, setClicked] = useState(false);
+//   const [inputValue, setInputValue] = useState();
+
+//   useEffect(() => {
+//     clicked ? setInnerText("clicked") : setInnerText("click me");
+//   }, [clicked]);
+
+//   function buttonClick() {
+//     setClicked(!clicked);
+//   }
+//   function onChangeFunction(event) {
+//     setInputValue(event.target.value);
+//   }
+
+//   return (
+//     <form>
+//       <label>Username</label>
+//       <StyleButton innerText={innerText} onClickHandler={buttonClick} />
+//       <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
+//     </form>
+//   );
+// };
+// export default StyledForm;
+import React, { useState } from "react";
 import StyledInput from "../StyledInput/StyledInput";
-import { useState, useEffect } from "react";
+import StyledButton from "../StyledButton/StyledButton";
 
-const StyledForm = (props) => {
-  const [innerText, setInnerText] = useState("");
-  const [clicked, setClicked] = useState(false);
-  const [inputValue, setInputValue] = useState();
+const CustomForm = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  useEffect(() => {
-    clicked ? setInnerText("clicked") : setInnerText("click me");
-  }, [clicked]);
-
-  function buttonClick() {
-    setClicked(!clicked);
+  function nameHandler(e) {
+    setName(e.target.value);
   }
-  function onChangeFunction(event) {
-    setInputValue(event.target.value);
+
+  function lastNameHandler(e) {
+    setLastName(e.target.value);
   }
 
   return (
-    <form>
-      <label>Username</label>
-      <StyleButton innerText={innerText} onClickHandler={buttonClick} />
-      <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
-    </form>
+    <div>
+      <StyledInput onChangeHandler={nameHandler} value={name} />
+      <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
+      <StyledButton innerText="Send" />
+    </div>
   );
 };
-export default StyledForm;
+
+export default CustomForm;
