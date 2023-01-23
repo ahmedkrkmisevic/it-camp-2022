814d02605d4f8b050472dc99a5fd121838f959f7
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-23 19:54:31 +0100

    react

--- my/src/components/StyledForm/StyledForm.jsx
+++ my/src/components/StyledForm/StyledForm.jsx
@@ -1,59 +1,59 @@
-// import React from "react";
-// import StyleButton from "../styleButton/styleButton";
-// import "./StyledForm.css";
-// import StyledInput from "../StyledInput/StyledInput";
-// import { useState, useEffect } from "react";
-
-// const StyledForm = (props) => {
-//   const [innerText, setInnerText] = useState("");
-//   const [clicked, setClicked] = useState(false);
-//   const [inputValue, setInputValue] = useState();
-
-//   useEffect(() => {
-//     clicked ? setInnerText("clicked") : setInnerText("click me");
-//   }, [clicked]);
-
-//   function buttonClick() {
-//     setClicked(!clicked);
-//   }
-//   function onChangeFunction(event) {
-//     setInputValue(event.target.value);
-//   }
-
-//   return (
-//     <form>
-//       <label>Username</label>
-//       <StyleButton innerText={innerText} onClickHandler={buttonClick} />
-//       <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
-//     </form>
-//   );
-// };
-// export default StyledForm;
-import React, { useState } from "react";
+import React from "react";
+import StyleButton from "../styleButton/styleButton";
+import "./StyledForm.css";
 import StyledInput from "../StyledInput/StyledInput";
-import StyledButton from "../StyledButton/StyledButton";
+import { useState, useEffect } from "react";
 
-const CustomForm = () => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
+const StyledForm = (props) => {
+  const [innerText, setInnerText] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [inputValue, setInputValue] = useState();
 
-  function nameHandler(e) {
-    setName(e.target.value);
+  useEffect(() => {
+    clicked ? setInnerText("clicked") : setInnerText("click me");
+  }, [clicked]);
+
+  function buttonClick() {
+    setClicked(!clicked);
   }
-
-  function lastNameHandler(e) {
-    setLastName(e.target.value);
+  function onChangeFunction(event) {
+    setInputValue(event.target.value);
   }
 
   return (
-    <div>
-      <StyledInput onChangeHandler={nameHandler} value={name} />
-      <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
-      <StyledButton innerText="Send" />
-    </div>
+    <form>
+      <label>Username</label>
+      <StyleButton innerText={innerText} onClickHandler={buttonClick} />
+      <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
+    </form>
   );
 };
+export default StyledForm;
+// import React, { useState } from "react";
+// import StyledInput from "../StyledInput/StyledInput";
+// import StyledButton from "../StyledButton/StyledButton";
 
-export default CustomForm;
+// const CustomForm = () => {
+//   const [name, setName] = useState("");
+//   const [lastName, setLastName] = useState("");
+
+//   function nameHandler(e) {
+//     setName(e.target.value);
+//   }
+
+//   function lastNameHandler(e) {
+//     setLastName(e.target.value);
+//   }
+
+//   return (
+//     <div>
+//       <StyledInput onChangeHandler={nameHandler} value={name} />
+//       <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
+//       <StyledButton innerText="Send" />
+//     </div>
+//   );
+// };
+
+// export default CustomForm;
 // console.log(1);
 // console.log(2);
