5c2a418b7142ed66cba46b02aff968de339dd4d8
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-24 18:54:39 +0100

    app-css

--- my/src/components/CustomForm/CustomForm.jsx
+++ my/src/components/CustomForm/CustomForm.jsx
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
+import StyledInput from "../StyledInput/StyledInput";
+import StyledButton from "../StyledButton/StyledButton";
 
+const CustomForm = () => {
+const CustomForm = (props) => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+ const CustomForm = () => {
+    <div>
+      <StyledInput onChangeHandler={nameHandler} value={name} />
+      <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
+      <StyledButton innerText='Send' />
+      <StyledButton
+        onClickHandler={() => props.sendData(name, lastName)}
+        innerText='Send'
+      />
+    </div>
+  );
+};
+export default CustomForm;
