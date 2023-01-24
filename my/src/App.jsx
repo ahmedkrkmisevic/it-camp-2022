5b22a2364b58f158b392d4591031cef9af34b148
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-24 18:47:55 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,76 +1,28 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Styleheader from "./components/styleheader/styleheader";
-import StyleButton from "./components/styleButton/styleButton";
-import StyledInput from "./components/StyledInput/StyledInput";
-import StyledForm from "./components/StyledForm/StyledForm";
+import CustomForm from "./components/CustomForm/CustomForm";
 import CustomDiv from "./components/CustomDiv/CustomDiv";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <CustomDiv>
-          <h4>{fullname}</h4>
-        </CustomDiv>
-        <CustomForm sendData={(name, lname) => dataGrabber(name, lname)} />
-      </header>
-    </div>
-  );
+  const [fullName, setFullName] = useState("");
 
-  function dataGrabber(name, lname) {
-    let fullname = name + " " + lname;
-    console.log(fullname);
-    FullName(fullname);
+  function dataGrabber(name, lastName) {
+    let fullName = name + " " + lastName;
+    console.log(fullName);
+    setFullName(fullName);
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <CustomForm />
         <CustomDiv>
-          <h4>{fullname}</h4>
+          <h4>{fullName}</h4>
         </CustomDiv>
-        <CustomForm sendData={(name, lname) => dataGrabber(name, lname)} />
+        <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
       </header>
     </div>
   );
 };
-
-// import React, { useState, useEffect } from "react";
-// import logo from "./logo.svg";
-// import React from "react";
-// import "./App.css";
-// import StyledButton from "./components/StyledButton/StyledButton";
-// import CustomForm from "./components/CustomForm/CustomForm";
-
-// const App = () => {
-//   const [innerText, setInnerText] = useState("");
-//   const [clicked, setClicked] = useState(false);
-
-//   useEffect(() => {
-//     clicked ? setInnerText("Clicked") : setInnerText("Click me");
-//   }, [clicked]);
-
-//   function buttonClick() {
-//     setClicked(!clicked);
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <StyledButton innerText={innerText} onClickHandler={buttonClick} />
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <CustomForm />
-//       </header>
-//     </div>
-//   );
-// };
+export default App;
