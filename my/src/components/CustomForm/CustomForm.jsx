b12490e9ccd97941cc26864d663c4f8d0628ddaf
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-24 18:56:52 +0100

    react

--- my/src/components/CustomForm/CustomForm.jsx
+++ my/src/components/CustomForm/CustomForm.jsx
@@ -3,20 +3,27 @@ import StyledInput from "../StyledInput/StyledInput";
 import StyledButton from "../StyledButton/StyledButton";
 
 const CustomForm = () => {
-const CustomForm = (props) => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const CustomForm = (props) => {
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
 
- const CustomForm = () => {
-    <div>
-      <StyledInput onChangeHandler={nameHandler} value={name} />
-      <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
-      <StyledButton innerText='Send' />
-      <StyledButton
-        onClickHandler={() => props.sendData(name, lastName)}
-        innerText='Send'
-      />
-    </div>
-  );
+    function nameHandler(e) {
+      setName(e.target.value);
+    }
+    function lastNameHandler(e) {
+      setLastName(e.target.value);
+    }
+    return (
+      <div>
+        <StyledInput onChangeHandler={nameHandler} value={name} />
+        <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
+        <StyledButton innerText="Send" />
+        <StyledButton
+          onClickHandler={() => props.sendData(name, lastName)}
+          innerText="Send"
+        />
+      </div>
+    );
+  };
 };
 export default CustomForm;
