7f59139ce564e961c97f6c167f05afc0932a7af3
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-25 21:35:47 +0100

    to do

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
 import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import CustomForm from "./components/CustomForm/CustomForm";
 import CustomDiv from "./components/CustomDiv/CustomDiv";
+import ToDo from "./components/ToDo/ToDo";
 
 const App = () => {
   const [fullName, setFullName] = useState("");
@@ -14,15 +15,18 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <CustomForm />
+    <div className='App'>
+      <header className='App-header'>
         <CustomDiv>
           <h4>{fullName}</h4>
         </CustomDiv>
         <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
       </header>
     </div>
+    <>
+      <ToDo />
+    </>
   );
 };
+
 export default App;
