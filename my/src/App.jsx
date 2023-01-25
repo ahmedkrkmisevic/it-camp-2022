acff015237003ffb5ed4af85ebed6acd7a7c060b
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-25 21:45:43 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React from "react";
 import "./App.css";
 import CustomForm from "./components/CustomForm/CustomForm";
 import CustomDiv from "./components/CustomDiv/CustomDiv";
