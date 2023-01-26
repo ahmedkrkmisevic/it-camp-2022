83f62d35c00c117f3bc4168df9cb4b7f1e4545df
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-26 16:35:03 +0100

    to do

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import React from "react";
+
 import "./App.css";
 import CustomForm from "./components/CustomForm/CustomForm";
 import CustomDiv from "./components/CustomDiv/CustomDiv";
--- my/src/components/CustomForm/CustomForm.jsx
+++ my/src/components/CustomForm/CustomForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import StyledInput from "../StyledInput/StyledInput";
-import StyledButton from "../StyledButton/StyledButton";
+import StyleButton from "../styleButton/styleButton";
 
 const CustomForm = () => {
   const CustomForm = (props) => {
@@ -17,8 +17,8 @@ const CustomForm = () => {
       <div>
         <StyledInput onChangeHandler={nameHandler} value={name} />
         <StyledInput onChangeHandler={lastNameHandler} value={lastName} />
-        <StyledButton innerText="Send" />
-        <StyledButton
+        <StyleButton innerText="Send" />
+        <StyleButton
           onClickHandler={() => props.sendData(name, lastName)}
           innerText="Send"
         />
