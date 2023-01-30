7c4a9b685bacf8b3332d90b4e79e89fe82596146
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-01-30 21:00:16 +0100

    react

--- my/src/components/CustomDiv/CustomDiv.jsx
+++ my/src/components/CustomDiv/CustomDiv.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import "./CustomDiv.css";
+const CustomDiv = ({
+  children,
+  width,
+  height,
+  borderRadius,
+  border,
+  padding,
+  marrgin,
+}) => {
+  const divWidth = width ? width : 100;
+  const divHeight = height ? height : 100;
+  const divBR = borderRadius ? borderRadius : 0;
+  const divBorder = border ? border : "1px solid black";
+  const divMargin = marrgin ? marrgin : 0;
+  const divPadding = padding ? padding : 0;
 
-const CustomDiv = ({ children }) => {
-  return <div>{children}</div>;
+  return;
+  <div
+    style={{
+      width: divWidth,
+      height: divHeight,
+      borderRadius: divBR,
+      border: divBorder,
+      margin: divMargin,
+      padding: divPadding,
+    }}
+  >
+    {children}
+  </div>;
 };
-
 export default CustomDiv;
