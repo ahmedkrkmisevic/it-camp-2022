0c3f5f002e8cf79644f49f71922f0978b04a39bd
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-01 20:57:35 +0100

    Grid

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,28 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import CustomForm from "./components/CustomForm/CustomForm";
-import CustomDiv from "./components/CustomDiv/CustomDiv";
-import ToDo from "./components/ToDo/ToDo";
+
+import GridCmp from "./components/Grid/Grid";
 
 const App = () => {
-  const [fullName, setFullName] = useState("");
-
-  function dataGrabber(name, lastName) {
-    let fullName = name + " " + lastName;
-    console.log(fullName);
-    setFullName(fullName);
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <CustomDiv>
-          <h4>{fullName}</h4>
-        </CustomDiv>
-        <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
-        <ToDo></ToDo>
-      </header>
-    </div>
+    <>
+      <GridCmp />
+    </>
   );
 };
 
--- my/src/components/CustomDiv/CustomDiv.jsx
+++ my/src/components/CustomDiv/CustomDiv.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "./CustomDiv.css";
+
 const CustomDiv = ({
   children,
   width,
@@ -7,33 +8,29 @@ const CustomDiv = ({
   borderRadius,
   border,
   padding,
-  marrgin,
+  margin,
 }) => {
   const divWidth = width ? width : 100;
   const divHeight = height ? height : 100;
   const divBR = borderRadius ? borderRadius : 0;
   const divBorder = border ? border : "1px solid black";
-  const divMargin = marrgin ? marrgin : 0;
+  const divMargin = margin ? margin : 0;
   const divPadding = padding ? padding : 0;
 
-  return;
-  <div
-    style={{
-      width: divWidth,
-      height: divHeight,
-      borderRadius: divBR,
-      border: divBorder,
-      margin: divMargin,
-      padding: divPadding,
-    }}
-  >
-    {children}
-  </div>;
+  return (
+    <div
+      style={{
+        width: divWidth,
+        height: divHeight,
+        borderRadius: divBR,
+        border: divBorder,
+        margin: divMargin,
+        padding: divPadding,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
+
 export default CustomDiv;
-// console.log(1);
-// console.log(1);
-// console.log(1);
-// console.log(1);
-// console.log(1);
-// console.log(1);
