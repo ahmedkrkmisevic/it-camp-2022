e7dbc9b4bedc5fb2930098ba80eebf8f00301b45
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-01 20:21:21 +0100

    Grid

--- my/src/components/Grid/Grid.jsx
+++ my/src/components/Grid/Grid.jsx
@@ -1,21 +1,12 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import CustomDiv from "../CustomDiv/CustomDiv";
 const GridCmp = () => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <CustomDiv></CustomDiv>
-        <Item>xs=8</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={8}>
-        <Item>xs=8</Item>
-      </Grid>
-    </Grid>
-  );
+  <Grid
+    container
+    direction="column"
+    justifyContent="space-between"
+    alignItems="center"
+  ></Grid>;
 };
 export default GridCmp;
