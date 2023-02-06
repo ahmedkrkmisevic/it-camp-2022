723fec13ce2a292d1eb3d674dabb8c65e84ed601
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-06 21:13:29 +0100

    react

---
+++ my/src/components/styledButton/StyledButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styledButton.css";
+import PrimaryButton from "../PrimaryButton/PrimaryButton";
+
+const StyledButton = (props) => {
+  return (
+    <button className='styledButton' onClick={props.onClickHandler}>
+      {props.innerText}
+    </button>
+  );
+const StyledButton = ({ onClickHandler, innerText }) => {
+  return <PrimaryButton onClick={onClickHandler}>{innerText}</PrimaryButton>;
+};
+
+export default StyledButton;
