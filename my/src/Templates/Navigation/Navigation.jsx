715a6722e262c774e22f1effe16dde01af3796d9
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-08 21:01:41 +0100

    react

--- my/src/Templates/Navigation/Navigation.jsx
+++ my/src/Templates/Navigation/Navigation.jsx
@@ -3,6 +3,12 @@ import { Grid } from "@mui/material";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
 
 const Navigation = () => {
-  return <Grid container direction="row"></Grid>;
+  return (
+    <Grid container direction="row">
+      <CustomDiv padding="0px 20px" border="0px" width="100%">
+        <TopRow></TopRow>
+      </CustomDiv>
+    </Grid>
+  );
 };
 export default Navigation;
