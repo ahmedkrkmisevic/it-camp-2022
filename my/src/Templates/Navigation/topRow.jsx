bb6ae9b2e762a018bfc635ee5a96d5261fef7f62
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-10 20:41:02 +0100

    react

--- my/src/Templates/Navigation/topRow.jsx
+++ my/src/Templates/Navigation/topRow.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Grid } from "@mui/material/Grid";
+import { Grid } from "@mui/material/Grid/Grid";
 import { colors } from "../../util/theme";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
-import PhoneIcon from "@mui/icons-material/";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const TopRow = () => {
   return (
-    <Grid container="row">
-      <CustomDiv
-        bgColor={colors.secondColor}
-        width="100%"
-        height="30px"
-        padding="0px 5 %"
-        border="0px"
-      >
-        <PhoneIcon />
-        <P>+381066317687</P>
-      </CustomDiv>
-    </Grid>
+    <CustomDiv
+      bgColor={colors.secondColor}
+      width="100%"
+      height="30px"
+      padding="0px 5 %"
+      border="0px"
+    >
+      <Grid container direction="row">
+        <CustomDiv width="50%" border="0px" bgColor="inherit">
+          <PhoneIcon />
+          <P>+381066317687</P>
+        </CustomDiv>
+        Grid
+        <CustomDiv width="50%" border="0px" bgColor="inherit"></CustomDiv>
+      </Grid>
+    </CustomDiv>
   );
 };
 export default TopRow;
