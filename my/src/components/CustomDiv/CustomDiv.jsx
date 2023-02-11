ef168bc98975dbb6129258d806910a82f494111a
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-11 19:00:28 +0100

    react

--- my/src/components/CustomDiv/CustomDiv.jsx
+++ my/src/components/CustomDiv/CustomDiv.jsx
@@ -13,17 +13,21 @@ const CustomDiv = ({
   display,
   justifyContent,
   justifyItems,
+  alignItems,
+  alignContent,
 }) => {
   const divWidth = width ? width : 100;
-  const divHeight = height ? height : 100;
+  const divHeight = height ? height : "auto";
   const divBR = borderRadius ? borderRadius : 0;
   const divBorder = border ? border : "1px solid black";
   const divMargin = margin ? margin : 0;
   const divPadding = padding ? padding : 0;
-  const backGroundColor = bgColor ? bgColor : "white";
+  const backGroundColor = bgColor ? bgColor : "inherit";
   const disp = display ? display : "block";
   const jc = justifyContent ? justifyContent : "flex-start";
   const ji = justifyItems ? justifyItems : "center";
+  const ai = alignItems ? alignItems : "center";
+  const ac = alignContent ? alignContent : "center";
 
   return (
     <div
@@ -38,6 +42,8 @@ const CustomDiv = ({
         display: disp,
         justifyContent: jc,
         justifyItems: ji,
+        alignContent: ac,
+        alignItems: ai,
       }}
     >
       {children}
