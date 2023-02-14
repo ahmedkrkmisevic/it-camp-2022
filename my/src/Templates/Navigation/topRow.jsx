34abedbc24f42076afd435b4238537660ce8f5c8
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-14 19:19:34 +0100

    react

--- my/src/Templates/Navigation/topRow.jsx
+++ my/src/Templates/Navigation/topRow.jsx
@@ -19,7 +19,7 @@ const TopRow = () => {
       border="0px"
     >
       <Grid container direction="row">
-        <Grid item md={6} lg={6} spacing={0}>
+        <Grid item md={6} lg={6} spacing={6}>
           <CustomDiv
             display="flex"
             alignItems="flex-start"
