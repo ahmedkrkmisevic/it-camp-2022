0911b5e5aa4ceb6264badc26081b0081fe34d702
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-15 20:58:25 +0100

    react

--- my/src/Templates/Navigation/MainBar.jsx
+++ my/src/Templates/Navigation/MainBar.jsx
@@ -1,6 +1,28 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import CustomDiv from "../../components/CustomDiv/CustomDiv";
+import Logo from "../../assets/images/logo.png";
+import { colors } from "../../util/theme";
+import { getScreenWidth } from "../../util/helpers";
+import { fontSize } from "../../util/theme";
 
 const MainBar = () => {
-  return <div>MainBar</div>;
+  return (
+    <CustomDiv
+      bgColor="white"
+      display="flex"
+      width="100%"
+      height="30px"
+      padding="0px 5%"
+      border="0px"
+    >
+      <Grid container direction="row">
+        <Grid item md={6} lg={6}>
+          <img src={Logo} alt="Logo" width="200" height="100" />
+        </Grid>
+        <Grid item md={6} lg={6}></Grid>
+      </Grid>
+    </CustomDiv>
+  );
 };
 export default MainBar;
