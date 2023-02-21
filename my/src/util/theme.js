396e197788a1f824dc57d7cc082ec215f7132b26
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-21 19:51:31 +0100

    react

--- my/src/util/theme.js
+++ my/src/util/theme.js
@@ -1,10 +1,9 @@
-export const accentColor = "#e88f23";
-export const secondColor = "#F0F0E9";
 export const colors = {
     accentColor: "#e88f23",
     secondColor: "#F0F0E9",
+    gray: "#828181",
+    lightGray: "#b3b1b1",
 };
-
 export const fontWeight = {
     light: "100",
     medium: "400",
@@ -14,6 +13,7 @@ export const fontWeight = {
 export const fontSize = {
     xSmall: "10px",
     small: "12px",
+    smallPlus: "14px",
     normal: "16px",
     medium: "18px",
     large: "24px",
