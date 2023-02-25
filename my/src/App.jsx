2a70f7f129df600346c713147a13cc8276244d60
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-25 19:36:47 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./Templates/Navigation/Navigation";
+import Slider from "./Templates/Slider/Slider";
+
 const App = () => {
   return (
     <>
-      <Navigation></Navigation>
+      <Navigation />
+      <Slider />
     </>
   );
 };
