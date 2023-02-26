b886ff92055990446cee127e5b760ffeb2b5e442
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-26 18:23:00 +0100

    search

--- my/src/Templates/Navigation/LinkBar.jsx
+++ my/src/Templates/Navigation/LinkBar.jsx
@@ -47,7 +47,7 @@ const LinkBar = () => {
             renderInput={(params) => (
               <TextField
                 {...params}
-                label="Pretrazite
+                label="Pretrazite"
                 InputProps={{
                   ...params.InputProps,
                   type: "search",
