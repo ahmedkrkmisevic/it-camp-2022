49f08536f145aa17b0bc4e8f5021954d85c155b6
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-02-27 19:32:17 +0100

    react

--- my/src/components/SimplifiedDiv/SimplifiedDiv.jsx
+++ my/src/components/SimplifiedDiv/SimplifiedDiv.jsx
@@ -1,17 +1,5 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel as SliderCarousel } from "react-responsive-carousel";
+const SimplifiedDiv = ({ style, children }) => (
+  <div style={style}>{children}</div>
+);
 
-const Carousel = ({ data }) => {
-  return (
-    <SliderCarousel>
-      {data.map((d) => (
-        <div>
-          <img src={d.imgSrc} />
-          <p className="legend">{d.imgText}</p>
-        </div>
-      ))}
-    </SliderCarousel>
-  );
-};
-
-export default Carousel;
+export default SimplifiedDiv;
