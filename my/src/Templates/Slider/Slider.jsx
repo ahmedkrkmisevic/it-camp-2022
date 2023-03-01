8d30a620b6c37c4a6e8c6a1b8d9d3e03344cb628
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-03-01 20:12:00 +0100

    react

--- my/src/Templates/Slider/Slider.jsx
+++ my/src/Templates/Slider/Slider.jsx
@@ -3,13 +3,23 @@ import girl1 from "../../assets/images/home/girl1.jpg";
 import girl2 from "../../assets/images/home/girl2.jpg";
 import girl3 from "../../assets/images/home/girl3.jpg";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import { Grid } from "@mui/material";
+import { getScreenWidth } from "../../util/helpers";
 
-const syles = {
-  sliderContainer: {
-    height: "200px",
-    margin: "0px 10%",
+const sliders = [
+  {
+    imgSrc: girl1,
+    imgText: "Zaova 1",
   },
-};
+  {
+    imgSrc: girl2,
+    imgText: "Svekrva 2",
+  },
+  {
+    imgSrc: girl3,
+    imgText: "Snaha 3",
+  },
+];
 
 const Slider = () => {
   const sliders = [
@@ -24,10 +34,38 @@ const Slider = () => {
     {
       imgSrc: girl3,
       imgText: "Image text 3",
+  const deviceWidth = getScreenWidth();
+
+  const setSliderMargin =
+    deviceWidth === "SM"
+      ? "0px"
+      : deviceWidth === "MD"
+      ? "0px 10px"
+      : "0px 10%";
+
+  const setPaddingTop =
+    deviceWidth === "SM" ? "10px" : deviceWidth === "MD" ? "20px" : "30px";
+
+  const styles = {
+    sliderContainer: {
+      width: "100%",
+      height: "200px",
+      paddingTop: setPaddingTop,
+      margin: setSliderMargin,
     },
   ];
+  };
 
   return <Carousel data={sliders} />;
+  return (
+    <Grid container direction='row'>
+      <Grid item xs={12} sm={12} md={12} lg={12}>
+        <SimplifiedDiv style={styles.sliderContainer}>
+          <Carousel data={sliders} />
+        </SimplifiedDiv>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Slider;
--- my/src/components/Carousel/Carousel.jsx
+++ my/src/components/Carousel/Carousel.jsx
@@ -1,6 +1,10 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as SliderCarousel } from "react-responsive-carousel";
-import { fontSize } from "../../util/theme";
+import Text from "../Text/Text";
+import { colors, fontSize } from "../../util/theme";
+import SimplifiedDiv from "../SimplifiedDiv/SimplifiedDiv";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const Carousel = ({ data }) => {
   const styles = {
@@ -11,16 +15,43 @@ const Carousel = ({ data }) => {
       right: 10,
     },
   };
+
   return (
-    <SliderCarousel showThumbs={false}>
+    <SliderCarousel>
+    <SliderCarousel
+      width='100%'
+      axis='horizontal'
+      showIndicators
+      showThumbs={false}
+      interval={1000}
+      autoPlay
+      infiniteLoop
+      showStatus={false}
+      showArrows={false}
+      swipeable
+    >
       {data.map((d) => (
         <div>
           <img src={d.imgSrc} />
-          <Text fontSize={fontSize.xLarge}>{d.imgText}</Text>
+          <p className='legend'>{d.imgText}</p>
+        <div key={d} style={{ height: "400px" }}>
+          <img
+            src={d.imgSrc}
+            style={{ height: "100%", objectFit: "contain" }}
+            alt={d.imgText}
+          />
+          <SimplifiedDiv style={styles.carouselText}>
+            <Text
+              fontSize={fontSize.xLarge}
+              color={colors.gray}
+              textAlign='left'
+            >
+              {d.imgText}
+            </Text>
+          </SimplifiedDiv>
         </div>
       ))}
     </SliderCarousel>
   );
 };
-
 export default Carousel;
