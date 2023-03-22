4e3ae1a393b567abaa3cee7333ae0ddad59ed13c
ahmedkrkmisevic <ahmedkrkmisevic10@gmail.com>
2023-03-22 20:07:03 +0100

    react

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -4,7 +4,23 @@ import Navigation from "./Templates/Navigation/Navigation";
 import Slider from "./Templates/Slider/Slider";
 import SimplifiedDiv from "./components/SimplifiedDiv/SimplifiedDiv";
 import ProductsHomePage from "./Templates/ProductsHomePage/ProductsHomePage";
-import SuggestedCarousel from "./Templates/SuggestedCarousel/SuggestedCarousel";
+
+import "./assets/fonts/Lato-Black.ttf";
+import "./assets/fonts/Lato-BlackItalic.ttf";
+import "./assets/fonts/Lato-Bold.ttf";
+import "./assets/fonts/Lato-BoldItalic.ttf";
+import "./assets/fonts/Lato-Italic.ttf";
+import "./assets/fonts/Lato-Light.ttf";
+import "./assets/fonts/Lato-LightItalic.ttf";
+import "./assets/fonts/Lato-Regular.ttf";
+import "./assets/fonts/Lato-Thin.ttf";
+import "./assets/fonts/Lato-ThinItalic.ttf";
+import "./assets/fonts/Montserrat-Black.ttf";
+import "./assets/fonts/Montserrat-BlackItalic.ttf";
+import "./assets/fonts/Montserrat-Bold.ttf";
+import "./assets/fonts/Montserrat-BoldItalic.ttf";
+import "./assets/fonts/Montserrat-ExtraBold.ttf";
+import "./assets/fonts/Montserrat-ExtraLight.ttf";
 
 const App = () => {
   return (
@@ -12,9 +28,7 @@ const App = () => {
       <Navigation />
       <Slider />
       <ProductsHomePage />
-      <SuggestedCarousel />
     </>
   );
 };
-
 export default App;
